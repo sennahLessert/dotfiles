38f5056f4a3cfd6a9777f1772b891a3e4adc4aa2
sennahLessert <sennahLessert@tutanota.com>
2017-04-17 19:52:38 +0200

    startup script is not beeing used any more

--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#start mopidy & ncmpcpp
-tmux new-session -d -s music
-tmux split-window -h -t music
-tmux select-pane -t 0
-tmux send-keys "mopidy" C-m
-tmux select-pane -t 1
-tmux send-keys "ncmpcpp" C-m
